15e60d0916ce80f13e646b26e4eb3f8529362f88
JAYD3V <W3Dojo@Gmail.com>
2022-06-18 13:16:55 -0700

    Added projects standard comment header (w/ publishing & licensing info) to all files that didn't already have the header

---
+++ src/test/engine-integrity.test.ts
@@ -0,0 +1,7 @@
+/**
+* @author Ajay Chambers <w3dojo@gmail.com>
+* @copyright COPYRIGHT 2022 - Andrew Jay Chambers
+* @license Apache-2.0 (Sort-of like MIT, but more defined)
+* @JAYD3V
+* @link Https://www.GitHub.com/JAYD3V/Alloy
+* */
